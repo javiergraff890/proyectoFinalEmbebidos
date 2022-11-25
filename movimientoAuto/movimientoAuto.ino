3f049ee7c4b8202ffe753b504ae763bbd81a9c0a
Franco Iurman <francoiurman@gmail.com>
2022-11-25 19:58:54 -0300

    agregadas funciones para la lectura de datos del sensor en movimientoAuto.ino

--- movimientoAuto/movimientoAuto.ino
+++ movimientoAuto/movimientoAuto.ino
@@ -1,12 +1,7 @@
-/*
-  Capitulo 20 de Arduino desde cero en Español.
-  Programa que utiliza el modulo controlador de motor L298N para hacer girar dos
-  motores hacia adelante y asi verificar la correcta conexion.
+#include <RH_ASK.h>
+#include <SPI.h>
 
-  Autor: bitwiseAr  
-
-*/
-
+RH_ASK rf_driver;
 
 int IN1 = 6;      // IN1 a pin digital 2
 int IN2 = 7;      // IN2 a pin digital 3
@@ -15,18 +10,33 @@ int IN3 = 5;      // IN3 a pin digital 7
 int IN4 = 4;      // IN4 a pin digital 8
 int ENB = 3;      // ENA a pin digital 9
 
+const int DETENIDO = 0;
+const int ADELANTE = 1;
+const int ATRAS = 2;
+const int IZQUIERDA = 3;
+const int DERECHA = 4;
+
 void setup(){
+
+
+  
+  rf_driver.init();
+  Serial.begin(57600);
+  
   pinMode(IN1, OUTPUT);   // IN1 como salida  
   pinMode(IN2, OUTPUT);   // IN2 como salida
   pinMode(ENA, OUTPUT);   // ENA como salida
   pinMode(IN3, OUTPUT);   // IN3 como salida
   pinMode(IN4, OUTPUT);   // IN4 como salida
   pinMode(ENB, OUTPUT);   // ENB como salida
+
+  
 }
 
+uint8_t buff[5];
+uint8_t bufflen = sizeof(buff);
+
 void loop(){
-
-
   avanzar();
   delay(2000);      // demora de 2 seg.
   avanzarDerecha();
@@ -115,4 +125,38 @@ void retrocederDerecha() //El motor A debe ir mas despacio
   digitalWrite(IN3, LOW); // IN3 en cero logico
   digitalWrite(IN4, HIGH);  // IN4 en uno logico  
 }
+
+//funcion que interpreta los valores recibidos del sensor
+
+void interpresarSenial(int num)
+{
+  switch(num){
+      case DETENIDO : {
+          detenerse();
+        }break;
+      case ADELANTE :{
+          avanzar();
+        }break;
+      case ATRAS :{
+        retroceder();
+        }break;
+      case IZQUIERDA:{
+        avanzarIzquierda();
+        }break;
+      case DERECHA :{
+        avanzarDerecha();
+        }break;
+    }
+}
+
+void recibirSenial()
+{
+   int numero;
+   if(rf_driver.recv(buff, &bufflen)){
+      numero = (int)buff;
+    } 
+}
+
+
+
   
